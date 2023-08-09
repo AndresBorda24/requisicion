b353fb0f407a7966b4d7719b3c89636c5cb63d1d
AndresBorda24 <anjart24@gmail.com>
2023-08-09 11:44:08 -0500

    Se agrego validacion de 'Enums' en request

--- app/Http/Requests/RequisicionRequest.php
+++ app/Http/Requests/RequisicionRequest.php
@@ -3,17 +3,25 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Enums\Tipo;
+use App\Enums\Motivo;
+use App\Enums\NivelEducativo;
+
 class RequisicionRequest extends BodyRequest
 {
     public function validateInsert(array $data): array
     {
         try {
+            $tipos = array_keys(Tipo::all());
+            $motivos = array_keys(Motivo::all());
+            $niveles = array_keys(NivelEducativo::all());
+
             return $this->validate($data, [
                 "area" => "required",
                 "cargo" => "required",
-                "tipo" => "required",
+                "tipo" => "required|in:".implode(",", $tipos),
                 "horas" => "required|integer",
-                "motivo" => "required",
+                "motivo" => "required|in:".implode(",", $motivos),
                 "sector" => "required",
                 "horario" => "required",
                 "jefe_id" => "required|integer",
@@ -23,7 +31,7 @@ class RequisicionRequest extends BodyRequest
                 "area_anios" => "required|integer",
                 "sector_anios" => "required|integer",
                 "conocimientos" => "required",
-                "nivel_educativo" => "required",
+                "nivel_educativo" => "required|in:".implode(",", $niveles),
             ]);
         } catch(\Exception $e) {
             throw $e;
