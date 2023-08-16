dde4568f8ee935942f8c94c5ae83729132cc1f26
AndresBorda24 <anjart24@gmail.com>
2023-08-16 11:10:12 -0500

    Ajuste a formulario

--- assets/jefes/index.js
+++ assets/jefes/index.js
@@ -3,6 +3,7 @@ import Alpine from "alpinejs";
 import form from "./comp/form";
 import grilla from "./comp/grilla";
 import ver from "@/partials/ver-req";
+import "@/partials/global-helpers.js";
 
 document.addEventListener("alpine:init", () => {
     Alpine.data("ReqForm", form);
--- templates/jefes/comp/req-form.php
+++ templates/jefes/comp/req-form.php
@@ -17,7 +17,7 @@ class="fixed-top bg-black bg-opacity-50 vh-100 vw-100 flex">
   @submit.prevent="save"
   class="small m-auto d-flex flex-column rounded overflow-auto" style="
     width: 70vw;
-    height: 80vh;
+    max-height: 80vh;
     max-width: 800px;
   ">
     <section class="bg-primary p-2">
@@ -30,28 +30,29 @@ class="fixed-top bg-black bg-opacity-50 vh-100 vw-100 flex">
       class="btn btn-sm btn-close btn-close-white float-end"></button>
     </section>
 
-    <section class="p-3 flex-grow-1 overflow-auto bg-body small text-primary-emphasis">
+    <section class="p-3 flex-grow-1 overflow-auto bg-light-subtle small">
       <p class="text-muted small">
         Todos los campos con (*) son requeridos.
       </p>
 
-      <h6>Informaci&oacute;n General</h6>
+      <h6 class="fw-bold">Informaci&oacute;n General</h6>
       <div class="mb-2 row g-0">
         <div class="p-1 col-md-9">
-          <label for="cargo" class="form-label text-muted small">Cargo*:</label>
+          <label for="cargo" class="form-label small">Cargo*:</label>
           <input
           type="text"
           required
-          placeholder="Auxiliar de Sistemas"
+          placeholder="Ej: Auxiliar de Sistemas"
           x-model="state.cargo"
           class="form-control form-control-sm" id="cargo">
         </div>
         <div class="p-1 col-md-3">
-          <label for="cantidad" class="form-label text-muted small"># Cargos*:</label>
+          <label for="cantidad" class="form-label small"># Cargos*:</label>
           <input
           min="1"
           required
           type="number"
+          placeholder="Ej: 1"
           x-model="state.cantidad"
           class="form-control form-control-sm" id="cantidad">
         </div>
@@ -59,7 +60,7 @@ class="fixed-top bg-black bg-opacity-50 vh-100 vw-100 flex">
 
       <div class="mb-2 row row-cols-1 row-cols-md-2 g-0">
         <div class="p-1">
-          <label for="motivo" class="form-label text-muted small">Motivo*:</label>
+          <label for="motivo" class="form-label small">Motivo*:</label>
           <select
           id="motivo"
           x-model="state.motivo"
@@ -73,7 +74,7 @@ class="fixed-top bg-black bg-opacity-50 vh-100 vw-100 flex">
         </div>
 
         <div class="p-1">
-          <label for="tipo" class="form-label text-muted small">Tipo*:</label>
+          <label for="tipo" class="form-label small">Tipo*:</label>
           <select
           id="tipo"
           x-model="state.tipo"
@@ -87,9 +88,9 @@ class="fixed-top bg-black bg-opacity-50 vh-100 vw-100 flex">
         </div>
       </div>
 
-      <div class="mb-4 row row-cols-1 row-cols-md-2 g-0">
+      <div class="mb-2 row row-cols-1 row-cols-md-2 g-0">
         <div class="p-1">
-          <label for="horario" class="form-label text-muted small">Horario*:</label>
+          <label for="horario" class="form-label small">Horario*:</label>
           <input
           type="text"
           id="horario"
@@ -99,105 +100,46 @@ class="fixed-top bg-black bg-opacity-50 vh-100 vw-100 flex">
         </div>
 
         <div class="p-1">
-          <label for="horas" class="form-label text-muted small">Horas Semanales*:</label>
+          <label for="horas" class="form-label small">Horas Semanales*:</label>
           <input
           type="number"
           min="0"
           id="horas"
           required
+          placeholder="Ej: 47"
           x-model="state.horas"
           class="form-control form-select-sm">
         </div>
       </div>
 
-      <h6>Cargo Nuevo</h6>
-      <div class="mb-2">
-        <label class="form-label text-muted small" for="nivel_educativo">
-          Educaci&oacute;n en (Nivel Acad&eacute;mico):
-        </label>
-        <select
-        x-model="state.nivel_educativo"
-        class="form-select form-select-sm"
-        id="nivel_educativo"
-        required>
-          <option selected="" hidden="" value="">-- Seleccionar --</option>
-          <?php foreach(\App\Enums\NivelEducativo::all() as $key => $value): ?>
-            <option value="<?= $key ?>"> <?= $value ?> </option>
-          <?php endforeach ?>
-        </select>
-      </div>
-
-      <div class="mb-4">
-        <label for="conv-conocimientos"
-        class="form-label small text-muted">Conocimientos:</label>
-        <textarea
-        x-model="state.conocimientos"
-        id="conv-conocimientos"
-        style="height: 110px;"
-        class="form-control form-control-sm"
-        placeholder="Detalla los conocimientos necesarios..."></textarea>
-      </div>
-
-
-      <h6>Experiencia Laboral</h6>
-      <div class="mb-2 row g-0">
-        <div class="p-1 col-12 col-md-7">
-          <label for="sector" class="form-label text-muted small">Sector*:</label>
-          <input
-          type="text"
-          id="sector"
-          x-model="state.sector"
-          required
-          class="form-control form-control-sm">
-        </div>
-
-        <div class="p-1 col-12 col-md-5">
-          <label for="sector_anios" class="form-label text-muted small">A&ntilde;os*:</label>
-          <input
-          min="0"
-          required
-          type="number"
-          id="sector_anios"
-          x-model="state.sector_anios"
-          class="form-control form-control-sm">
-        </div>
-      </div>
-
-      <div class="mb-2 row g-0">
-        <div class="p-1 col-12 col-md-7">
-          <label for="area" class="form-label text-muted small">Area*:</label>
-          <input
-          id="area"
-          required
-          type="text"
-          x-model="state.area"
-          class="form-control form-control-sm">
-        </div>
-
-        <div class="p-1 col-12 col-md-5">
-          <label for="area_anios" class="form-label text-muted small">A&ntilde;os*:</label>
-          <input
-          min="0"
-          required
-          type="number"
-          id="area_anios"
-          x-model="state.area_anios"
-          class="form-control form-control-sm">
-        </div>
-      </div>
-
       <div class="mb-4">
         <label class="form-label small text-muted" for="funciones">
-          Funciones principales del cargo
+          Funciones
         </label>
         <textarea
-        required
         id="funciones"
-        style="height: 110px;"
+        style="height: 100px;"
         x-model="state.funciones"
+        placeholder="Si quieres, escribe las principales funciones del cargo..."
         class="form-control form-control-sm"
         ></textarea>
       </div>
+
+      <hr>
+
+      <div class="mb-4">
+        <label class="form-label small text-muted" for="observacion">
+          Observaci&oacute;n:
+        </label>
+        <textarea
+        id="observacion"
+        style="height: 50px;"
+        placeholder="Si es necesario, deja una observaci&oacute;n..."
+        x-model="state.observacion"
+        class="form-control form-control-sm"
+        ></textarea>
+      </div>
+
     </section>
 
     <section class="bg-secondary d-flex justify-content-end p-2">
