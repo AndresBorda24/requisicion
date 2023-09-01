e49b3d6f86ddd90805c3e71adb2b55ddcd1132f5
AndresBorda24 <anjart24@gmail.com>
2023-09-01 08:40:27 -0500

    Se removieron los colores de los estados

--- templates/partials/grilla/item.php
+++ templates/partials/grilla/item.php
@@ -5,12 +5,7 @@ class="small align-middle position-relative">
   <td x-text="item?.area_nombre" class="text-nowrap"></td>
   <td x-text="item?.cargo" class="text-nowrap"></td>
   <td x-text="item?.created_at" class="text-nowrap px-2"></td>
-  <td :class="['p-0 position-relative', stateClass]">
-    <span class="position-absolute top-0 end-0 h-100 d-flex align-items-center
-    ps-2 bg-white bg-opacity-50 text-black" style="width: 97%;">
-      <span x-html="item?._state"></span>
-    </span>
-  </td>
+  <td class="text-nowrap" x-html="item?._state"></td>
   <td class="position-sticky end-0">
     <button
     class="btn btn-sm btn-warning py-0"
