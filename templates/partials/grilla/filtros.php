65c8642863dd0e99829c0612f84d8ecb66d3f6ad
AndresBorda24 <anjart24@gmail.com>
2023-09-07 10:22:11 -0500

    Pequeño ajuste a inputs en filtros

--- templates/partials/grilla/filtros.php
+++ templates/partials/grilla/filtros.php
@@ -14,10 +14,11 @@ class="position-relative">
   class="text-bg-secondary position-absolute rounded-1 top-100 end-0 p-2 shadow mt-1 border border-warning-subtle">
     <div class="mb-2 small">
       <label for="filter-state" class="form-label m-0">Estado:</label>
-      <select x-model="filters.state" id="filter-state" class="form-select form-select-sm">
-        <option value="">Todos</option>
+      <select x-model="filters.state" id="filter-state"
+      class="bg-opacity-10 bg-white border-opacity-25 border-white form-select form-select-sm text-light text-uppercase">
+        <option value="" class="text-dark">Todos</option>
         <?php foreach (\App\Enums\Estados::all() as $key => $value): ?>
-          <option value="<?= $key ?>"><?= $value ?></option>
+          <option value="<?= $key ?>" class="text-dark"><?= $value ?></option>
         <?php endforeach ?>
       </select>
     </div>
@@ -25,10 +26,11 @@ class="position-relative">
     <?php if($this->isRoute("req.th")): // Solo si es la vista de TH  ?>
     <div class="mb-2 small">
       <label for="filter-area" class="form-label m-0">&Aacute;rea:</label>
-      <select x-model="filters.area" id="filter-area" class="form-select form-select-sm">
-        <option value="">Todas</option>
+      <select x-model="filters.area" id="filter-area"
+      class="bg-opacity-10 bg-white border-opacity-25 border-white form-select form-select-sm text-light text-uppercase">
+        <option value="" class="text-dark">Todas</option>
         <template x-for="area in areas()" :key="area.id">
-          <option :value="area.id" x-text="area.nombre"></option>
+          <option :value="area.id" x-text="area.nombre" class="text-dark"></option>
         </template>
       </select>
     </div>
@@ -40,7 +42,7 @@ class="position-relative">
       x-model.debounce.600="filters.cargo"
       type="text" id="filter-cargo"
       @keyup="$el.value = $el.value.toUpperCase()"
-      class="text-uppercase form-control form-control-sm">
+      class="bg-opacity-10 bg-white border-opacity-25 border-white form-control form-control-sm text-light text-uppercase">
     </div>
   </div>
 </details>
