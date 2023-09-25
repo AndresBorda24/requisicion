620c3e3cc549de18b1c6e4a409361c67b53a0443
AndresBorda24 <anjart24@gmail.com>
2023-09-25 17:01:23 -0500

    Meta store

---
+++ assets/stores/meta-store.js
@@ -0,0 +1,26 @@
+import Alpine from "alpinejs";
+import { getMetaInfo } from "@/requests/ExtraRequests";
+
+document.addEventListener("alpine:init", () => {
+    Alpine.store("META", {
+        _: {},
+
+        /** @param {String} key */
+        get( key ) {
+            if (!this._ || ! this._.hasOwnProperty(key)) return undefined;
+
+            return this._[ key ];
+        },
+
+        async fetchInfo() {
+            const [data, err] = await getMetaInfo();
+            if (err) return;
+
+            this._ = data;
+        }
+    });
+});
+
+document.addEventListener('alpine:initialized', async () => {
+    Alpine.store("META").fetchInfo();
+});
